38f3a6ae47a836f89dd657a8244f7f12b522e911
pietro o destruidor de motanhas <pietro.merlo@escola.pr.gov.br>
2026-06-30 10:38:22 +0000

    sim

--- lista1/ex8.js
+++ lista1/ex8.js
@@ -1,1 +1,15 @@
-const input = require('readline-sync')
+const input = require('readline-sync');
+let tabuleiro = ""
+let tamanho = Number(input.question("Digite o tamanho do tabuleiro:"));
+for (let linha = 0; linha < tamanho; linha++) {
+
+    for (let coluna = 0; coluna < tamanho; coluna++) {
+ if ((linha + coluna) % 2 === 0) {
+    tabuleiro += " ";
+} else {
+  tabuleiro += "#";
+}
+}
+    tabuleiro += "\n";
+}
+console.log(tabuleiro);
